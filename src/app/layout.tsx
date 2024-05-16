950d5a0a916696d5a635aed9445be9ab8addd588
dreadbeard11 <cocoboy1337@gmail.com>
2024-05-16 15:58:17 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import localFont from 'next/font/local'
+
  
 // Font files can be colocated inside of `pages`
 const oggRegular = localFont({ src: './Ogg-Regular.woff' })
