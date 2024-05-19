c2c14704c3a2cf023510fead233604d7c42b770b
dreadbeard11 <cocoboy1337@gmail.com>
2024-05-18 18:15:44 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,39 @@ import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "@/lib/api";
 
+/*
+/////////
+
+import markdownToHtml from "@/lib/markdownToHtml";
+
+import { PostBody } from "@/app/_components/post-body";
+import { PostHeader } from "@/app/_components/post-header";
+
+export default async function Post({ params }: Params) {
+  const post = getPostBySlug(params.slug);
+
+  if (!post) {
+    return notFound();
+  }
+
+  const content = await markdownToHtml(post.content || "");
+
+  return (
+    <main>
+      <Container>
+        
+        <article className="mb-32">
+         
+          <PostBody content={content} />
+        </article>
+      </Container>
+    </main>
+  );
+}
+
+////////
+*/
+
 export default function Index() {
   const allPosts = getAllPosts();
 
