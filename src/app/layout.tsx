c8c2aa43156a208f5e68f9976510918b09df5d9e
dreadbeard11 <cocoboy1337@gmail.com>
2024-05-20 20:13:45 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,9 +61,24 @@ export default function RootLayout({
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       </head>
       <body className={inter.className}>
-        <div className="xheader font-display text-sm p-4">Rechtsanwalt Wiazewicz</div>
+       <div className="drawer drawer-end">
+  <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+  <div className="drawer-content">
+    <div className=`{$oggRegular.className} xheader font-display text-sm p-4`>Rechtsanwalt Wiazewicz</div>
         <div className="min-h-screen">{children}</div>
         <Footer />
+    <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary">Open drawer</label>
+  </div> 
+  <div className="drawer-side">
+    <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
+    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+      {/* Sidebar content here */}
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+    </ul>
+  </div>
+</div>
+        
       </body>
     </html>
   );
