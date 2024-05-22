5446288cda275157ff6662c9d209886a2f48c89e
dreadbeard11 <cocoboy1337@gmail.com>
2024-05-22 11:14:52 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import Container from "@/app/_components/container";
-import { HeroPost } from "@/app/_components/hero-post";
+
 import { Intro } from "@/app/_components/intro";
 
 
 export default function Index() {
-
-
-  // const content = await markdownToHtml(heroPost.title || "");
 
   return (
     <main>
