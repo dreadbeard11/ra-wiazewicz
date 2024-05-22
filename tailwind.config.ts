9d92fe8cb0a9a43333bbb00ef1275827e6ae1868
dreadbeard11 <cocoboy1337@gmail.com>
2024-05-21 19:09:33 -0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,90 +1,44 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./utils/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   plugins: [require("daisyui")],
-  darkTheme: "darkx",
-  darkMode: ["selector", "[data-theme='darkx']"],
-  // DaisyUI theme colors
-  daisyui: {
-    themes: [
-      {
-        lightx: {
-          primary: "#f0f",
-          "primary-content": "#fff",
-          secondary: "#f08",
-          "secondary-content": "#fff",
-          accent: "#0ff",
-          "accent-content": "#00f",
-          neutral: "#888",
-          "neutral-content": "#ffffff",
-          "base-100": "#ffffff",
-          "base-200": "#eee",
-          "base-300": "#ddd",
-          "base-content": "#00f",
-          info: "#08f",
-          success: "#0f0",
-          warning: "#F80",
-          error: "#F00",
-
-          "--rounded-btn": "9999rem",
-
-          ".tooltip": {
-            "--tooltip-tail": "6px",
-          },
-          ".link": {
-            textUnderlineOffset: "2px",
-          },
-          ".link:hover": {
-            opacity: "80%",
-          },
-        },
-      },
-      {
-        darkx: {
-          primary: "#f0f",
-          "primary-content": "#fff",
-          secondary: "#f08",
-          "secondary-content": "#fff",
-          accent: "#0ff",
-          "accent-content": "#00f",
-          neutral: "#888",
-          "neutral-content": "#ffffff",
-          "base-100": "#ffffff",
-          "base-200": "#eee",
-          "base-300": "#ddd",
-          "base-content": "#00f",
-          info: "#08f",
-          success: "#0f0",
-          warning: "#F80",
-          error: "#F00",
-
-          "--rounded-btn": "9999rem",
-
-          ".tooltip": {
-            "--tooltip-tail": "6px",
-            "--tooltip-color": "oklch(var(--p))",
-          },
-          ".link": {
-            textUnderlineOffset: "2px",
-          },
-          ".link:hover": {
-            opacity: "80%",
-          },
-        },
-      },
-    ],
-  },
   theme: {
     extend: {
-      boxShadow: {
-        center: "0 0 12px -2px rgb(0 0 0 / 0.05)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      animation: {
-        "pulse-fast": "pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      colors: {
+        "accent-1": "#FAFAFA",
+        "accent-2": "#EAEAEA",
+        "accent-7": "#333",
+        success: "#0070f3",
+        cyan: "#79FFE1",
+      },
+      spacing: {
+        28: "7rem",
+      },
+      letterSpacing: {
+        tighter: "-.04em",
+      },
+      fontSize: {
+        "5xl": "2.5rem",
+        "6xl": "2.75rem",
+        "7xl": "4.5rem",
+        "8xl": "6.25rem",
+      },
+      boxShadow: {
+        sm: "0 5px 10px rgba(0, 0, 0, 0.12)",
+        md: "0 8px 30px rgba(0, 0, 0, 0.12)",
       },
     },
-  },
+  }
+
 };
 export default config;
