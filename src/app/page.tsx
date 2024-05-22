32ee693f9690913da15511976f2f816bb21248e3
dreadbeard11 <cocoboy1337@gmail.com>
2024-05-22 11:02:07 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,10 @@
 import Container from "@/app/_components/container";
 import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
-import { MoreStories } from "@/app/_components/more-stories";
-import { getAllPosts } from "@/lib/api";
 
-import markdownToHtml from "@/lib/markdownToHtml";
-import { PostBody } from "@/app/_components/post-body";
-
-/*
-/////////
-
-
-import { PostHeader } from "@/app/_components/post-header";
-
-export default async function Post({ params }: Params) {
-  const post = getPostBySlug(params.slug);
-
-  if (!post) {
-    return notFound();
-  }
-
-  const content = await markdownToHtml(post.content || "");
-
-  return (
-    <main>
-      <Container>
-        
-        <article className="mb-32">
-         
-          <PostBody content={content} />
-        </article>
-      </Container>
-    </main>
-  );
-}
-
-////////
-*/
 
 export default function Index() {
-  const allPosts = getAllPosts();
 
-  const heroPost = allPosts[0];
-
-  const morePosts = allPosts.slice(1);
 
   // const content = await markdownToHtml(heroPost.title || "");
 
@@ -52,15 +13,7 @@ export default function Index() {
       <Container>
         <Intro />
      
-        <HeroPost
-          title={heroPost.title}
-          coverImage={heroPost.coverImage}
-          date={heroPost.date}
-          author={heroPost.author}
-          slug={heroPost.slug}
-          excerpt={heroPost.excerpt}
-        />
-      
+
       </Container>
     </main>
   );
