2456ce91e7af1ac12a60a2c1bfd37d4d3b7e8f21
dreadbeard11 <cocoboy1337@gmail.com>
2024-05-22 11:05:55 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,49 @@ export default function Index() {
       <Container>
         <Intro />
      
+Rechtsanwalt Wiazewicz
+Tätigkeitsbereiche
+
+    Arbeitsrecht
+    Ausländerrecht
+    Asylrecht
+    Familienrecht
+    Kitarecht
+    Mietrecht
+    Strafverteidigung
+
+Korrespondenzsprachen
+
+deutsch, englisch, französisch, niederländisch
+Anschrift
+
+Stubenrauchstr. 70
+12161 Berlin Friedenau
+Tel: 030 44049982
+Mobil: 0172 3905208
+Email: ra@wiazewicz.de
+Information nach dem Telemediengesetz
+
+Umsatzsteuernummer: 210838591
+
+Herr Wiazewicz ist Mitglied der Rechtsanwaltskammer Berlin und in Berlin zugelassen; Postulationsfähig vor allen Amts,-Land und Oberlandesgerichten/Kammergericht.
+
+Die Berufshaftpflichtversicherung von Rechtsanwalt Wiazewicz (Versicherungs-Nr. 03.053.5511 50) besteht bei der Gothaer Versicherungsbank VVaG, Gothaer Allee 1, 50969 Köln.
+
+Es gelten die folgenden berufsrechtlichen Regelungen:
+(a) Bundesrechtsanwaltsordnung (BRAO)
+(b) Rechtsanwaltsvergütungsgesetz (RVG)
+(c) Berufsordnung (BerufsO)
+(e) Berufsregeln der Rechtsanwälte der Europäischen Gemeinschaft
+(einsehbar auf dieser Homepage :www.brak.de)
+
+Aufsichtsbehörde im Sinne des § 6 TDG ist die Rechtsanwaltskammer Berlin.
+
+Haftungsauschluss
+
+Die Informationen auf diesen Internetseiten wurden nach bestem Wissen zusammengestellt. Sie dienen der allgemeinen Information. Die hier gegebenen Informationen stellen keine rechtliche Beratung dar. Eine Haftung aufgrund der hier gegebenen allgemeinen Hinweise ist daher ausgeschlossen.
+
+Als Service für den Benutzer enthalten diese Seiten außerdem Verweise (Links) zu Internetseiten anderer Anbieter. Auf die Gestaltung und die Inhalte der gelinkten Seiten habe ich keinen Einfluss. Ich übernehme keinerlei Gewähr oder Haftung für den Inhalt, die Aktualität, Korrektheit, Vollständigkeit, Qualität oder Quelle der auf diesen Seiten dargebotenen Informationen oder davon heruntergeladener Dateien. Ich bin in keiner Weise verantwortlich für die Internetseiten Dritter und distanziere mich von deren Inhalten ausdrücklich.
 
       </Container>
     </main>
