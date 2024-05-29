81db360ff69b92bac789c79065a1effcd60898ac
dreadbeard11 <cocoboy1337@gmail.com>
2024-05-29 14:46:11 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Container from "@/app/_components/container";
 import { Intro } from "@/app/_components/intro";
 // import ThemeSwitch from "@/app/_components/ThemeSwitch";
 
-import { MoonIcon, DevicePhoneMobile } from "@heroicons/react/24/outline";
+import { MoonIcon, DevicePhoneMobileIcon } from "@heroicons/react/24/outline";
 
 export default function Index() {
 
@@ -37,7 +37,7 @@ deutsch, englisch, französisch, niederländisch
 <div>Stubenrauchstr. 70</div>
 <div>12161 Berlin Friedenau</div>
 <div>Tel: 030 44049982 </div>
-<div><DevicePhoneMobile className="h-5 w-5" />Mobil: 0172 3905208 </div>
+<div><DevicePhoneMobileIcon className="h-5 w-5" />Mobil: 0172 3905208 </div>
 <div>Email: ra@wiazewicz.de </div>
   
 </div>
