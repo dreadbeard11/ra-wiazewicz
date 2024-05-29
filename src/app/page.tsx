3ae08c1f0c7f07d1a4161956c05b9ec3eade7783
dreadbeard11 <cocoboy1337@gmail.com>
2024-05-29 02:59:31 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ deutsch, englisch, französisch, niederländisch
 <div>Email: ra@wiazewicz.de </div>
   
 </div>
-<div className="border border-2 rounded-xl border-current bg-[#00f2] p-4">
+<div className="border border-2 rounded-xl border-current bg-[#00f1] p-4">
         
 <h2 className="font-bold text-sm">Information nach dem Telemediengesetz</h2>
 
