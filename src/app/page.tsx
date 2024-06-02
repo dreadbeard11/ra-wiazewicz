b0a9a422d857ebd52304b12437628d120f613525
dreadbeard11 <cocoboy1337@gmail.com>
2024-06-01 21:39:59 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ deutsch, englisch, französisch, niederländisch
   <div className="space-x-8 flex flex-row pt-8">
 
       
-<div>  <div>
+<div>  
               
   <PhoneIcon className="w-4 inline" />
   Tel:</div>
