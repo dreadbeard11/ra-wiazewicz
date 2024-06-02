1357e8c7e5da807935cf79e8a9e2a48a7022927a
dreadbeard11 <cocoboy1337@gmail.com>
2024-06-02 07:37:08 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  title: `Rechtsanwalt Wiazewicz`,
+  description: ``,
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
   },
