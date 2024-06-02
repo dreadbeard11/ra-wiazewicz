128fdf737e5ee87ac9580c7d584dfcb35d8b7d3f
dreadbeard11 <cocoboy1337@gmail.com>
2024-06-02 15:40:20 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Index() {
   return (
     <main>
       <Container>
-        <div className="space-y-18 md:w-[900px] max-w-[1200px] text-lg">
+        <div className="space-y-24 md:w-[900px] max-w-[1200px] text-lg">
           <div>
             <h2>Tätigkeitsbereiche</h2>
             <ul>
