38568858cf66a4afa02dd0aaf0ebd7085722e794
dreadbeard11 <cocoboy1337@gmail.com>
2024-06-02 20:35:53 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Index() {
               <div>
                 <div>
                   {" "}
-                  <div className="w-6 h-6 p-[2px] rounded-full bg-base-content text-base-100">
+                  <div className="w-6 h-6 p-[2px] items-center rounded-full bg-base-content text-base-100">
                  <PhoneIcon className="w-4 h-4 "/>
                   </div>
                   Tel:
