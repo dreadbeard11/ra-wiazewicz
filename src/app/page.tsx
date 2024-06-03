b5466a81988d07644a28379330157d4424a6d0d3
dreadbeard11 <cocoboy1337@gmail.com>
2024-06-03 16:08:13 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,12 +41,12 @@ export default function Index() {
             <div className="sm:space-x-8 sm:flex sm:flex-row pt-8">
               <div>
                 <div>
-                  <div className="w-6 h-6 flex items-center justify-center rounded-full bg-base-content text-base-100">
+                  <div className="inline w-6 h-6 flex items-center justify-center rounded-full bg-base-content text-base-100">
                  <PhoneIcon className="w-4 h-4"/>
                   </div>
                   Tel:
                 </div>
-                <div className="inline">030 44049982 </div>
+                <div>030 44049982 </div>
               </div>
 
               <div>
