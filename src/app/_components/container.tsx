fc3cf81b3272805ba2e6ed2961c12e129bec8ecd
dreadbeard11 <cocoboy1337@gmail.com>
2024-06-07 20:42:43 -0600

    Update container.tsx

--- src/app/_components/container.tsx
+++ src/app/_components/container.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 
 const Container = ({ children }: Props) => {
-  return <div className="container mx-auto px-6">{children}</div>;
+  return <div className="container">{children}</div>;
 };
 
 export default Container;
