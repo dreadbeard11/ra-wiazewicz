a9aefa3f435ccef967b1ccbd62e5a408cf8ff9b0
dreadbeard11 <cocoboy1337@gmail.com>
2024-06-10 09:32:51 -0600

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,15 +1,15 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Rechtsanwalt Wiazewicz',
+  author: 'mac11fish',
+  headerTitle: 'Rechtsanwalt Wiazewicz',
+  description: 'A one-page site created with Next.js and Tailwind.css',
   language: 'en-us',
-  theme: 'dark', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
-  socialBanner: '/static/images/twitter-card.png',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://ra-wiazewicz.vercel.app/',
+  siteRepo: 'https://github.com/dreadbeard11/ra-wiazewicz',
+  siteLogo: '/favicon/safari-pinned-tab.svg',
+  socialBanner: '/assets/OG_ra-wiazewicz.jpg',
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
   github: 'https://github.com',
